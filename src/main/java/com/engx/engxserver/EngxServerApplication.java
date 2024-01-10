3bd2c517f84949612c31140f9840efeba67a7d12
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2024-01-10 07:33:41 +0700

    init

---
+++ src/main/java/com/engx/engxserver/EngxServerApplication.java
@@ -0,0 +1,13 @@
+package com.engx.engxserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngxServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngxServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/engx/engxserver/EngxServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engx.engxserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngxServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
