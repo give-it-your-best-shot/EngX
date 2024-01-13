1e463d3ee553665344dece30910d9f88f9900d8a
Trương Công Hoàng Long <truongconghoanglong@gmail.com>
2024-01-13 22:01:51 +0700

    fix cors

--- src/main/java/com/engx/engxserver/EngxServerApplication.java
+++ src/main/java/com/engx/engxserver/EngxServerApplication.java
@@ -4,27 +4,12 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EngxServerApplication {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
-    }
-
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedMethods("*")
-                        .allowedOrigins("${CLIENT_URL}")
-                        .allowedHeaders("*");
-                ;
-            }
-        };
     }
 
     public static void main(String[] args) {
