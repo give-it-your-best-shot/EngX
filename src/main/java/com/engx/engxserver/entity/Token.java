febe2ad80f55374a6e825dba8ff8ca7983adb313
Trương Công Hoàng Long <truongconghoanglong@gmail.com>
2024-01-26 15:05:35 +0700

    fix: token

--- src/main/java/com/engx/engxserver/entity/Token.java
+++ src/main/java/com/engx/engxserver/entity/Token.java
@@ -6,6 +6,7 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -22,8 +23,8 @@ import lombok.NoArgsConstructor;
 public class Token {
 
     @Id
-    @GeneratedValue
-    public Integer id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
 
     @Column(unique = true)
     public String token;
